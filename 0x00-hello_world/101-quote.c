fa96728dc07cbf1ec3c8360be8d9435ef367f890
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-19 20:27:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (2, message, sizeof(message) - 1);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	fputs(s, stderr);
 	return (1);
 }
