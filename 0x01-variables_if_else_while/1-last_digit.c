b1e3dc2b4c38e443bd8a3310349edbf124d501c3
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-20 23:37:35 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %d is %d ", n, n % 10 );
+	printf("Last digit of %d is %d ", n, n % 10);
 
 	if (n % 10 > 5)
 	{
@@ -24,10 +24,10 @@ int main(void)
 	{
 		printf("and is 0\n");
 	}
-	else 
+	else
 	{
-		printf("less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	}
 
-	return(0);
+	return (0);
 }
