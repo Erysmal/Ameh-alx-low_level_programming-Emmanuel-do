90cdd86d82aca17dfe6bc7862b4b056054fc3d73
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-24 15:11:31 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is lowercase else retun 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
