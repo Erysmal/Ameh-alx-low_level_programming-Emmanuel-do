19e99693aac7c00f5cc6e3c9690f75fecc3ba792
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-24 16:43:26 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digitvof a number
+ *
+ * @n: Charaacter needed to be checked
+ *
+ * Return: Value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+
+	last_digit = n % 10;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
