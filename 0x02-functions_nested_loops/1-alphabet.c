d541a5c2585e7f2b00b71d3cb798c049111d2566
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-24 11:09:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		_putchar(letters);
+	}
+
+	_putchar('\n');
+
+}
