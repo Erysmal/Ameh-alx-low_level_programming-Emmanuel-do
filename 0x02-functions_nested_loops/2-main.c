72ca5d984fe6acaa8555ae29996351acaa9e4d9e
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-24 14:38:31 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: Alawyas 0 (sucess)
+ */
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet in 10 new lines
+ */
+void print_alphabet_x10(void)
+{
+	int a, i;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+
+		}
+		_putchar('\n');
+	}
+}
