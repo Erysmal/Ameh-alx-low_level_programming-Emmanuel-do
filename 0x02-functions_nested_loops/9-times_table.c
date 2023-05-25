6745cd600372187c8f6beb74e2f97a2d52b2c4a5
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-25 02:17:30 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Function that print 9 time tab from 0
+ *
+ * Return: Alwats 0.
+ */
+
+void times_table(void)
+{
+	int row, column;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			int results = row * column;
+
+			printf("%2d", results);
+
+			if (column != 9)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}
