00f3490b1bf1829a0688f7dd8f84c5bd70769ccc
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-24 23:58:39 -0700

    6-abs.c

---
+++ 0x01-variables_if_else_while/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Function that computes absolute values
+ *
+ * @n: The character to be checked
+ *
+ * Return: The value of abs
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+
+}
