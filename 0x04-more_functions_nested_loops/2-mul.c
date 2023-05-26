ab333493ede5309cdec0d11c968be10d4caeaa76
Erysmal <benedictemmanuel7110@gmail.com>
2023-05-26 12:22:08 -0700

    2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiply two integers
+ *
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: the (result) of the multiplicatiom
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
