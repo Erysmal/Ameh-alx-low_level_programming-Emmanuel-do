82991e31ae5bd1e28865b17de43aa344e992292d
Erysmal <benedictemmanuel7110@gmail.com>
2023-06-05 07:29:22 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function that copy memory area
+ * @dest: Memory where is stored
+ * @src: Memory where is copied
+ * @n: number of bytes
+ * Return: (dest)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n++;
+	}
+
+	return (dest);
+}
